cc9c81ff6efd78851587acdcb232899477143168
Keenah <kvancamp@mail.com>
2022-03-18 02:06:57 -0500

    defined menu method

--- lib/red_rocks/cli.rb
+++ lib/red_rocks/cli.rb
@@ -1,19 +1,29 @@
 class RedRocks::CLI
 
-    def call
+    def call 
         list_events
         menu
     end
 
-    def list_events
+    def list_events #displays list of events 
         puts "\nEvents:"
         @events = RedRocks::Event.all
-        @events.each.with_index(1) do |event, x|
+        @events.each.with_index(1) do |event, x| 
             puts "#{x}. '#{event.title}' by #{event.artist} -  #{event.date}, #{event.timestamp}"
         end
     end
     
     def menu
+
+        puts "\nHello and Welcome to the RedRocks CLI"
+
+        input = nil
+        while input != "exit"
+            
+            puts "\nEnter show #, 'list' to display the list again, or enter 'exit' to quit"
+            input = gets.strip.downcase 
+
+        end
     end 
     
 end
