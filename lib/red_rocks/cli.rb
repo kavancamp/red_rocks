d5eefa78094767ca5fe9cd9509cf2aec1b24b5dc
Keenah <kvancamp@mail.com>
2022-03-18 15:44:43 -0500

    commenting out code to play around a bit

--- lib/red_rocks/cli.rb
+++ lib/red_rocks/cli.rb
@@ -3,11 +3,22 @@ class RedRocks::CLI
     def call 
         list_events
         menu
+        months
+    end
+
+    def months #get and return months
+        #temporary data
+        
+    end
+
+    def month_prompt #prompt user for month choice
+
     end
 
     def list_events #displays list of events 
-        puts "\nEvents:"
-        @events = RedRocks::Event.all
+
+        @events = RedRocks::Even
+        t.all
         @events.each.with_index(1) do |event, x| 
             puts "#{x}. '#{event.title}' by #{event.artist} -  #{event.date}, #{event.timestamp}"
         end
@@ -19,11 +30,32 @@ class RedRocks::CLI
 
         input = nil
         while input != "exit"
-            
             puts "\nEnter show #, 'list' to display the list again, or enter 'exit' to quit"
             input = gets.strip.downcase 
-
-        end
-    end 
-    
+            #Capture input using gets
+            #.strip method to remove any new lines or leading and trailing whitespace.
+         # if input.to_i > 0 && input.to_1 < 3
+               # the_event = @events[input.to_i-1]
+              #  puts "\n\t'#{the_event.title}' by #{the_event.artist} - #{the_event.timestamp}, #{the_event.date}"
+              #  puts "\n\t#{the_event.description.capitalize!}" if the_event.description != nil
+              #  default = "https://www.redrocksonline.com/events/?view=calendar"
+               # if @the_event.url
+                  #  doc = Nokogiri::HTML(open("#{the_event.url}"))
+                  #  event = doc.search("div.article-body p").text
+                  #  puts"\n"
+                  #  puts event
+                  #  puts "\n"
+                    #puts the_event.url
+               # else   
+                   # puts"\n Take a look at https://www.redrocksonline.com/events/?view=calendar for more information on the venue and events!"
+               # end
+               # elsif input == 'exit'
+               #     puts 'goodbye'
+              #  elsif input == 'list'
+              #      list_events
+         #   else 
+             #   puts "\n Invalid Entry, please enter 'list', 'exit', or enter the number of an Event need more information on"
+           # end
+       # end
+    #end 
 end
