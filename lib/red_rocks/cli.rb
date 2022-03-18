3a873330625f94891944a4e6b8806a71cdb5c316
Keenah <kvancamp@mail.com>
2022-03-18 01:50:25 -0500

    updated list_events

--- lib/red_rocks/cli.rb
+++ lib/red_rocks/cli.rb
@@ -6,6 +6,11 @@ class RedRocks::CLI
     end
 
     def list_events
+        puts "\nEvents:"
+        @events = RedRocks::events.all
+        @events.each.with_index(1) do |event, x|
+            puts "#{x}. '#{event.title}' by #{event.artist} -  #{event.date}, #{event.timestamp}"
+        end
     end
     
     def menu
