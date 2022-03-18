b185d53ea4c7cb1c855885e1502a1012d5cd429a
Keenah <kvancamp@mail.com>
2022-03-18 01:50:09 -0500

    created classes

--- lib/red_rocks.rb
+++ lib/red_rocks.rb
@@ -1,8 +1,9 @@
-#environment file
+#environment
 
 require_relative "red_rocks/version"
 require_relative "red_rocks/cli"
 require_relative "red_rocks/scraper"
+require_relative "red_rocks/event"
 
 module RedRocks
   class Error < StandardError; end
---
+++ lib/red_rocks/scraper.rb
@@ -0,0 +1,4 @@
+class RedRocks::Scraper 
+
+
+end 
