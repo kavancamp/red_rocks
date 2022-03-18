828e739f92634fed254555dec4272bc3b9ada65e
Keenah <kvancamp@mail.com>
2022-03-18 01:57:07 -0500

    created scraper

--- lib/red_rocks/scraper.rb
+++ lib/red_rocks/scraper.rb
@@ -1,4 +1,15 @@
-class RedRocks::Scraper 
+class RedRocks::Scraper
 
-
-end 
+    def self.scrape
+      doc = Nokogiri::HTML(open("https://www.redrocksonline.com/events/?view=calendar"))
+  
+      doc.css("div.entry.event-icons.clearfix").each do |event|
+        title = event.css("h3").text
+        artist = event.css("h4").text
+        date = event.css("span.m-date-value.m-value").text
+        timestamp = event.css("span.m-doors").text.gsub("Doors","").strip
+        url = event.css("div.buttons a").attr("href").text
+        RedRocks::Event.new(title, artist, date, timestamp, url)
+      end
+    end
+  end
