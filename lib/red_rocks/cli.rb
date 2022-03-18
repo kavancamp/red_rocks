b59134ab8132967dec6e7ffcea3e77e0eaf15435
Keenah <kvancamp@mail.com>
2022-03-18 01:54:05 -0500

    fixed typo

--- lib/red_rocks/cli.rb
+++ lib/red_rocks/cli.rb
@@ -7,7 +7,7 @@ class RedRocks::CLI
 
     def list_events
         puts "\nEvents:"
-        @events = RedRocks::events.all
+        @events = RedRocks::Event.all
         @events.each.with_index(1) do |event, x|
             puts "#{x}. '#{event.title}' by #{event.artist} -  #{event.date}, #{event.timestamp}"
         end
