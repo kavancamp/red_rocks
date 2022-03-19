10a433655738d6c1747c319486d3a8ee9c09e16d
Keenah <kvancamp@mail.com>
2022-03-18 19:07:46 -0500

    Update month.rb

--- lib/red_rocks/month.rb
+++ lib/red_rocks/month.rb
@@ -1,4 +1,16 @@
 class RedRocks::Month
-    
+    attr_accessor :name
 
+    @@all = []
+
+    #class method
+    def self.all 
+        @@all
+    end
+
+    def initialize(name)
+        @name = name
+        
+    end
+    
 end
