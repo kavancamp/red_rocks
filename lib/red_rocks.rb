ebcce929e411722076930fdfe2441207759eea28
Keenah <kvancamp@mail.com>
2022-03-18 19:06:45 -0500

    added require

--- lib/red_rocks.rb
+++ lib/red_rocks.rb
@@ -4,6 +4,7 @@ require_relative "red_rocks/version"
 require_relative "red_rocks/cli"
 require_relative "red_rocks/scraper"
 require_relative "red_rocks/event"
+require_relative "red_rocks/month"
 
 module RedRocks
   class Error < StandardError; end
