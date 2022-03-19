0499328118dcf8998f134932cfe684725d1a9583
Keenah <kvancamp@mail.com>
2022-03-18 19:36:23 -0500

    added nokogiri

--- lib/red_rocks.rb
+++ lib/red_rocks.rb
@@ -1,10 +1,12 @@
 #environment
-require 'pry'
 require_relative "red_rocks/version"
 require_relative "red_rocks/cli"
 require_relative "red_rocks/scraper"
 require_relative "red_rocks/event"
 require_relative "red_rocks/month"
+
+require 'pry'
+require 'nokogiri'
 
 module RedRocks
   class Error < StandardError; end
